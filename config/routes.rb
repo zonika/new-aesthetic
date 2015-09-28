1e2fc1cfde262e08badcff811c2590b1f810b446
zonika <zchodosh@conncoll.edu>
2015-09-28 16:56:37 -0400

    add home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: "users/sessions", registrations: 'users/registrations' }
+  get '/', to: 'home#index'
+  get '/about', to: 'home#about', as: 'about'
 end
