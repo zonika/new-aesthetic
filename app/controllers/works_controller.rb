a0a36674b55d2d8a2c83ac818a40b0840d8df8e3
zonika <zchodosh@conncoll.edu>
2015-09-29 11:01:23 -0400

    image upload using paperclip

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,9 +2,17 @@ class WorksController < ApplicationController
 
   def create
     @user = current_user
+    @work = @user.works.create(work_params)
+    redirect_to index
   end
   def index
     @user = current_user
+  end
+
+  private
+
+  def work_params
+    params.require(:work).permit(:name, :image)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: "users/sessions", registrations: 'users/registrations' }
   resources :users, only: [:show, :edit, :update, :destroy] do
-    resources :works, only: [:index, :edit, :update, :destroy]
+    resources :works, only: [:index, :create, :edit, :update, :destroy]
   end
   get '/', to: 'home#index'
   get '/about', to: 'home#about', as: 'about'
