d70b43d193e88b8f4e0b99dce1ea6452de8f38a5
EJLarcs <ejlarcombe@gmail.com>
2015-09-29 09:13:48 -0400

    Portfolio controller and basic routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: "users/sessions", registrations: 'users/registrations' }
   get '/', to: 'home#index'
   get '/about', to: 'home#about', as: 'about'
+  get '/myportfolio', to: 'portfolio#home', as: 'portfolio'
 end
