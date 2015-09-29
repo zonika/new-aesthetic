32bcf02dab7626b3d665918c66fc8954693d4f8b
Gerardo Veltri <gerardo.veltri@gmail.com>
2015-09-29 14:41:15 -0400

    blah

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150929144526) do
+ActiveRecord::Schema.define(version: 20150929173751) do
 
   create_table "tags", force: :cascade do |t|
     t.string   "name"
