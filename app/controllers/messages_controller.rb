a70b712b01048401005f4044372bb293a40e5098
EJLarcs <ejlarcombe@gmail.com>
2015-10-05 17:46:32 -0400

    mesaging directly from user profile

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,6 +5,10 @@ class MessagesController < ApplicationController
     @chosen_recipient = User.find_by(id: params[:to].to_i) if params[:to]
   end
 
+  def message_user
+
+  end
+
   def create
     recipients = User.where(id: params['recipients'])
     conversation = current_user.send_message(recipients, params[:message][:body], params[:message][:subject]).conversation
