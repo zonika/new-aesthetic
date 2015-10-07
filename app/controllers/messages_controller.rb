589fd59debb634ec5e09cec17c8121f0278c3356
EJLarcs <ejlarcombe@gmail.com>
2015-10-07 15:58:00 -0400

    beautiful messaging modal:

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,13 +5,9 @@ class MessagesController < ApplicationController
     @chosen_recipient = User.find_by(id: params[:to].to_i) if params[:to]
   end
 
-  def message_user
-  end
-
   def create
     recipients = User.where(id: params['recipients'])
     conversation = current_user.send_message(recipients, params[:message][:body], params[:message][:subject]).conversation
-    flash[:success] = "Message has been sent!"
-    redirect_to conversation_path(conversation)
+    flash[:modal_success] = "Message has been sent!"
   end
 end
